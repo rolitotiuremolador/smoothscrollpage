cd7e8150b38524522977a43f634840dcafd166d8
Rolito Remolador <rolito_remolador09@hotmail.com>
2023-07-08 21:15:51 +0300

    Feature 3, smootscroll using polyfills by adding its framework and adding javascript.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 // Option 2 - jQuery Smooth Scroll
-$('.navbar a').on('click', function(e) {
-    // console.log(this.hash);
-    if(this.hash !== ''){
-        e.preventDefault();
+// $('.navbar a').on('click', function(e) {
+//     // console.log(this.hash);
+//     if(this.hash !== ''){
+//         e.preventDefault();
 
-        const hash = this.hash;
+//         const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800);
-    }
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//     }
+// });
+
+// Option 3 - smooth scroll using polyfills 
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800
 });
